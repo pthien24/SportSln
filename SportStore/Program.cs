6dd9d6937180ae3faab1f5482560ced5e0af65ef
pthien24 <72865946+pthien24@users.noreply.github.com>
2023-11-19 20:18:07 +0700

    update product order ,admin ,admin order

--- SportStore/Program.cs
+++ SportStore/Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<StoreDBContext>(opts =>
-{
+
+builder.Services.AddDbContext<StoreDBContext>(opts => {
     opts.UseSqlServer(
         builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
